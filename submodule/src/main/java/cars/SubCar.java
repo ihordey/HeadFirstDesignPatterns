04eeca0f2d3728d19988d80144cbe2c9c45bbbc5
Vanya <Ivan_Hordey@epam.com>
2017-10-14 16:08:36 +0300

    Revert "configure submodule and jacoco"
    This reverts commit 7a0b083

--- submodule/src/main/java/cars/SubCar.java
+++
@@ -1,9 +0,0 @@
-package cars;
-
-import lombok.Value;
-
-@Value
-public class SubCar {
-    private  String subModel;
-    private Long waight;
-}
--- submodule/src/test/java/cars/SubCarTest.java
+++
@@ -1,6 +0,0 @@
-package cars;
-
-public class SubCarTest {
-    public void getWaight() throws Exception {
-    }
-}
